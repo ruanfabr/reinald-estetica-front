e9e997792854cc9dc714acfc53366091146fd0a4
ruanfabr <devictor.ruan@gmail.com>
2026-01-05 23:54:55 -0300

    progredindo com contatos, melhorando botões e estilização

--- app/contatos/page.tsx
+++ app/contatos/page.tsx
@@ -1,29 +1,51 @@
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp,
+    FaInstagram
+ } from "react-icons/fa";
+ import { BsTelephone } from "react-icons/bs";
 
 
 export default function Contatos(){
 
+    const styleSocialButtons = 'border-2 p-3 hover:bg-black hover:text-white'
+
     return(
         <div className=''>
-            <div className="pt-2 flex flex-col">
-                <div className="flex flex-col gap-5">
-                    <h1 className="title">Nosso Contato</h1>
+            <div className="py-10"/>
+            <div className="pt-2 flex flex-col px-10 gap-8">
+                <h1 className="title m-auto ">Nossos Contatos</h1>
 
-                    <div className="flex gap-5">
-                        <p>whats</p>
-                        <p>insta</p>
-                    </div>
-                    <div className="flex  justify-center">
-                        <div className="flex border-1 border-black px-3 py-2">
-                            <FaWhatsapp className="text-[30px]"/>
-                            <p className="h-full text-[20px]">(85) 98548-2531</p>
-                        </div>
-                    </div>
+                <p className="font-semibold">
+                    Sede <br/>
+
+                    Av. Frei Cirilo, 3840 - Messejana,<br/>
+                    Fortaleza - CE, 60840-285
+                </p>
+
+                <div className="flex gap-5 items-center">
+                    <a href="https://wa.me/5585989387167" target="_blank"
+                    className={styleSocialButtons}>
+                        <FaWhatsapp className="text-[30px]"/>
+                    </a>
+
+                    <a href="https://www.instagram.com/ruan.com.r/" target="_blank"
+                    className={styleSocialButtons}>
+                        <FaInstagram className="text-[30px]"/>
+                    </a>
                 </div>
 
-                <div>
-                    eae
+
+                <div className="flex w-fit border-2 border-black px-4 py-3 gap-3 rounded-md">
+                    <BsTelephone className="text-[30px]"/>
+                    <p className="h-full text-[20px] font-semibold">(85) 98548-2531</p>
                 </div>
+
+                <div className="pt-5">
+                    <h2 className="text-[27px] font-bold text-wrap">
+                        Entre em contato conosco
+                    </h2>
+
+                </div>
+
             </div>
         </div>
     )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,28 @@
 'use client'
 import Link from "next/link";
 import { useState } from "react";
-
+import { TfiMenu } from "react-icons/tfi";
 
 export default function Navbar() {
 
     const [seeLinks, setSeeLinks] = useState(false)
 
     // max-[850px]:bg-gray-500
-    const style_subPages = "flex items-center h-full px-2 hover:text-white"
+    const style_subPages = "flex items-center h-full px-3 hover:text-white"
 
     return(
-        <nav className="flex justify-between px-14 h-18 bg-[#ACB1F9] relative">
+        <nav className="flex justify-between px-14 max-[850px]:px-5 h-18 bg-[#ACB1F9] relative">
             <Link href='/' className="flex items-center h-full px-2 hover:text-white font-bold text-[18px]">
                 Homepage
             </Link>
 
-            <button className="border-2 border-black p-2 hidden max-[850px]:block relative" onClick={() => setSeeLinks(!seeLinks)}>
-                oi
+            <button className="p-2 hidden max-[850px]:block relative text-3xl" onClick={() => setSeeLinks(!seeLinks)}>
+                <TfiMenu/>
             </button>
 
-            <div className={`gap-7 h-full min-[851px]:flex 
+            <div className={`gap-7 h-full flex min-[851px]:flex 
                 ${seeLinks? 'block':'hidden'}
-                max-[850px]:absolute max-[850px]:bg-gray-500 max-[850px]:top-18 max-[850px]:right-2 max-[850px]:h-fit max-[850px]:gap-5 max-[850px]:flex-col
+                max-[850px]:absolute max-[850px]:bg-gray-500 max-[850px]:top-18 max-[850px]:right-2 max-[850px]:h-fit max-[850px]:gap-5 max-[850px]:flex-col max-[850px]:py-5 max-[850px]:rounded-md
                 `}>
                 
                 <Link href='/contatos' className={style_subPages} onClick={() => setSeeLinks(!seeLinks)}>
